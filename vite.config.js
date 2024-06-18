e76180435c39674c463ad2e0b638b52e43b8f29f
carrot-tofu <songzw@broada.com>
2024-06-18 14:07:39 +0800

    feat: vite.config 基础配置

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,14 @@ export default defineConfig({
   },
   server: {
     // open: '/docs/index.html',
+    // 指定dev sever的端口号，默认为5173
+    port: 3000,
+    // 自动打开浏览器运行以下页面
+    open: '/'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src') // 资源路径别名配置
+    }
   }
 })
