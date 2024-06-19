d28c5b59e328602c732b434ac37ba823f84f411a
carrot-tofu <songzw@broada.com>
2024-06-19 10:16:21 +0800

    fix: 解决Modal.confirm暗色主题未换肤问题（使用antd useModal contextHolder）

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { goto } from '@/api'
-import { Button, theme } from 'antd'
+import { Button, Modal, theme } from 'antd'
 import './index.less'
+import { logout } from '../../api'
 
 const { useToken } = theme
 
 function Home() {
   // 创建路由钩子
-  const navigate = useNavigate()
 
   // 获取Design Token
   const { token } = useToken()
+
+  const [modal, contextHolder] = Modal.useModal()
+
+  const exit = () => {
+    modal.confirm({
+      title: '是否退出登录',
+      onOk() {
+        logout()
+      }
+    })
+  }
 
   return (
     <div className="P-home">
@@ -29,12 +40,16 @@ function Home() {
         <Button
           type="primary"
           onClick={() => {
-            navigate('/login')
+            exit()
           }}
         >
           返回登录
         </Button>
       </div>
+      {
+        // 这是最终解决Modal.method跟随换肤的关键，contextHolder在组件DOM中随便找个地方放就行
+        contextHolder
+      }
     </div>
   )
 }
